d422a774f6ff914ff6079e145f590cdaa840e900
sebastian <sebastianbeltran341@gmail.com>
2021-01-04 15:47:54 -0500

    proyecto finalizado

---
+++ config/email.js
@@ -0,0 +1,6 @@
+module.exports = {
+    user: 'd79d8037481a2b',
+    pass: '4d225e32ccc040',
+    host: 'smtp.mailtrap.io',
+    port: '2525'
+}
--- routes/index.js
+++ routes/index.js
@@ -5,68 +5,99 @@ const vacantesController = require('../controllers/vacantesController');
 const usuariosController = require('../controllers/usuariosController');
 const authController = require('../controllers/authController');
 
-module.exports = () => {
 
+
+module.exports = () => {
     router.get('/', homeController.mostrarTrabajos);
 
-    //-------------------VACANTES---------------------------//
-    //Crear vacantes
+    // Crear Vacantes
     router.get('/vacantes/nueva',
         authController.verificarUsuario,
-        vacantesController.formularioNuevaVacante);
+        vacantesController.formularioNuevaVacante
+    );
     router.post('/vacantes/nueva',
         authController.verificarUsuario,
         vacantesController.validarVacante,
-        vacantesController.agregarVacante);
-    // Mostrar vacantee
+        vacantesController.agregarVacante
+    );
+
+    // Mostrar Vacante (singular)
     router.get('/vacantes/:url', vacantesController.mostrarVacante);
-    //Editar vacante
+
+    // Editar Vacante
     router.get('/vacantes/editar/:url',
         authController.verificarUsuario,
-        vacantesController.formEditarVacante);
+        vacantesController.formEditarVacante
+    );
     router.post('/vacantes/editar/:url',
         authController.verificarUsuario,
         vacantesController.validarVacante,
-        vacantesController.editarVacante);
+        vacantesController.editarVacante
+    );
 
-    //Eliminar vacantes
+    // Eliminar Vacantes
     router.delete('/vacantes/eliminar/:id',
-        vacantesController.eliminarVacante);
+        vacantesController.eliminarVacante
+    );
 
-    //-------------------CUENTAS---------------------------//
-    //crear cuenta
+    // Crear Cuentas
     router.get('/crear-cuenta', usuariosController.formCrearCuenta);
     router.post('/crear-cuenta',
         usuariosController.validarRegistro,
-        usuariosController.crearUsuario);
-    //Autenticar usuarios
+        usuariosController.crearUsuario
+    );
+
+    // Autenticar Usuarios
     router.get('/iniciar-sesion', usuariosController.formIniciarSesion);
     router.post('/iniciar-sesion', authController.autenticarUsuario);
-
-    //Cerrar sesion
+    // cerrar sesion
     router.get('/cerrar-sesion',
         authController.verificarUsuario,
-        authController.cerrarSesion);
+        authController.cerrarSesion
+    );
 
-    //--------------------------PANEL ADMINISTRACION---------------------------//
+    // Resetear password (emails)
+    router.get('/reestablecer-password', authController.formReestablecerPassword);
+    router.post('/reestablecer-password', authController.enviarToken);
+
+    // Resetear Password ( Almacenar en la BD )
+    router.get('/reestablecer-password/:token', authController.reestablecerPassword);
+    router.post('/reestablecer-password/:token', authController.guardarPassword);
+
+
+    // Panel de administración
     router.get('/administracion',
         authController.verificarUsuario,
-        authController.mostrarPanel);
+        authController.mostrarPanel
+    );
 
-    //Editar Perfil
+    // Editar Perfil
     router.get('/editar-perfil',
         authController.verificarUsuario,
-        usuariosController.formEditarPerfil);
+        usuariosController.formEditarPerfil
+    );
     router.post('/editar-perfil',
         authController.verificarUsuario,
-       // usuariosController.validarPerfil,
-       usuariosController.subirImagen,
-        usuariosController.editarPerfil);
+        // usuariosController.validarPerfil,
+        usuariosController.subirImagen,
+        usuariosController.editarPerfil
+    )
 
-        //Recibir mensajes de candidatos
-        router.post('/vacantes/:url',
+    // Recibir Mensajes de Candidatos
+    router.post('/vacantes/:url',
         vacantesController.subirCV,
-        vacantesController.contactar);
+        vacantesController.contactar
+    );
+
+    // Muestra los candidatos por vacante
+    router.get('/candidatos/:id',
+        authController.verificarUsuario,
+        vacantesController.mostrarCandidatos
+    )
+
+    // Buscador de Vacantes
+    router.post('/buscador', vacantesController.buscarVacantes);
+
 
     return router;
 }
